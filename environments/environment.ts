12bfbb59950722a1dd240a566336617a5ce18bee
Dejan Markovic <58048670+dejooo98@users.noreply.github.com>
2023-10-31 15:16:19 +0100

    Update environment.ts

--- environments/environment.ts
+++ environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:3000/api/v1/' //-- LOCALHOST
-  apiUrl: 'https://angular-ecommerce-backend.herokuapp.com/api/v1/' //-- HOSTED BACKEND
+  //apiUrl: 'https://angular-ecommerce-backend.herokuapp.com/api/v1/' //-- HOSTED BACKEND
+  apiUrl: 'https://back-end-angular-shop.vercel.app/api/v1/' //-- HOSTED BACKEND
 };
 
 /*
